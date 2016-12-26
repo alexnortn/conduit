30a5f41c59bbd9a0d2ec66bd10f30774c7e1db66
Alex Norton <alexnortn@gmail.com>
2016-12-26 14:41:40 -0500

    added town names to the stack

--- parse-mma.js
+++ parse-mma.js
@@ -1,1 +1,23 @@
-for (var i=0; i < panels.length; i++) { var links = panels[i].getElementsByClassName("linkRow"); console.log(links); }
+// Alex Norton 2106
+// Scripts for scraping town names from MMA --> Massachusetts Municiple Association
+
+// Usage:
+// 		Go to: https://www.mma.org/city-and-town-web-sites
+// 		Open console, paste following code
+
+
+
+var panels = document.getElementById("slider").getElementsByClassName("panel")
+var paths = [];
+
+for (var i=0; i < panels.length; i++) {
+	var links = panels[i].getElementsByClassName("linkRow");
+	for (var j = 0; j < links.length; j++) {
+		var link = links[j].getElementsByClassName("comLink")[0];
+		if (link.getElementsByTagName("a").length) { 
+			var URL = link.getElementsByTagName("a")[0].getAttribute("href");
+			console.log(URL);
+			paths.push(URL);
+		}
+	}
+}
